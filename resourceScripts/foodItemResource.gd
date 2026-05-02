ac8ca810b20931e6620d7638f19b7fdeb643eac1
Csongor-Zsolt <hczsolt98@gmail.com>
2026-05-02 07:28:17 +0100

    Added some basic logic for rewinding and put item processing on the resource to help with rewinding later

--- resourceScripts/foodItemResource.gd
+++ resourceScripts/foodItemResource.gd
@@ -5,7 +5,10 @@ class_name FoodItem
 @export var spoilTime: float = 10.0
 @export var foodName: String
 @export var spriteTexture: Texture2D
-@export var usageTable: Dictionary[String,bool]
+@export var usageTable: Dictionary[String,bool] #station name -> new item
+@export var productTable: Dictionary[String, FoodItem] #station name -> product
+@export var undercookedTable: Dictionary[String, FoodItem]
+@export var overcookedTable: Dictionary[String, FoodItem]
 
 
 # Called when the node enters the scene tree for the first time.
--- scripts/foodItem.gd
+++ scripts/foodItem.gd
@@ -1,20 +1,22 @@
-extends Node
+extends Node2D
 
 var spoilTimer : Timer
 @export var foodData: FoodItem = FoodItem.new()
 @export var spoilInto: FoodItem = FoodItem.new()
+##var inStation: bool = false not sure we'll need this yet
 
 const REWINDMAX: int = 300
 var rewinding: bool
 var rewindState: Array[FoodItem] = [FoodItem.new()]
 var rewindPosition: Array[Vector2]
+var rewindTimer: Array[float]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spoilTimer = $SpoilTimer
 	spoilTimer.wait_time = foodData.spoilTime
 	##self.name = foodData.foodName
-	if foodData.spoiling == true:
+	if foodData.spoiling == false:
 		spoilTimer.paused = true
 		spoilTimer.wait_time = 409600.0
 	$Sprite2D.texture = foodData.spriteTexture
@@ -24,17 +26,42 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+# Using physics_process for fixed amount of updates per second
 func _physics_process(delta: float) -> void:
+		##Update Record
 	if not rewinding:
+		if rewindState.size()>=REWINDMAX:
+			rewindState.pop_front()
 		rewindState.append(foodData)
-		rewindPosition.append()
+		if rewindPosition.size()>=REWINDMAX:
+			rewindPosition.pop_front()
+		rewindPosition.append(global_position)
+		if rewindTimer.size()>=REWINDMAX:
+			rewindTimer.pop_front()
+		rewindTimer.append(spoilTimer.time_left)
+		##Rewind Record
 	else:
-		pass
+		var lastState = rewindState.pop_back()
+		var lastPosition = rewindPosition.pop_back()
+		var lastTimer = rewindTimer.pop_back()
+		if lastState != foodData:
+			pass ##Swap back food data
+		global_position = lastPosition
+		spoilTimer.wait_time = lastTimer
+		
 	
-	spoilTimer.time_left
+
+func pauseSpoiling() -> void:
+	spoilTimer.paused = true
+
+func resumeSpoiling()->void:
+	if foodData.spoiling==true:
+		spoilTimer.paused = false
 
 func startRewind()->void:
+	pauseSpoiling()
 	rewinding = true;
 	
 func stopRewind()->void:
+	resumeSpoiling()
 	rewinding = false;
